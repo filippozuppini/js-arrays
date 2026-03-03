a7170feb98a6d69b15ea8f6a84321e4f26493ec3
filippozuppini <zuppini.filippo@gmail.com>
2026-03-03 22:58:50 +0100

    3 and 4 exercise

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -22,15 +22,20 @@ console.log(teachers);
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 
 
-const lastTeacher = teachers[teachers.length -1];
-console.log(teachers.shift[lastTeacher]);
-
+const lastTeacher = teachers.pop();
+console.log(lastTeacher);
 
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+
+
+const firstTeacher = teachers.shift();
+console.log(firstTeacher);
+
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+
+
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
