a834677fbe6bd445d9959950563043fa5e55ddd5
filippozuppini <zuppini.filippo@gmail.com>
2026-03-03 22:44:48 +0100

    Second exercise

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -15,9 +15,17 @@ console.log(fourthTeacher);
 
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+teachers[4] = 'Patrick';
+console.log(teachers);
+
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+
+
+const lastTeacher = teachers[teachers.length -1];
+console.log(teachers.shift[lastTeacher]);
+
+
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = null;
